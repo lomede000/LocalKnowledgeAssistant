97fa5ed7b38a04504b0549209dfc78752a1c172b
Abgar Uyar <abgar-uyar@web.de>
2026-06-19 07:44:17 +0200

    Grundfunktionen(daten in einem ordner ausgeben)

--- LocalKnowledgeAssistant/Program.cs
+++ LocalKnowledgeAssistant/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LocalKnowledgeAssistant.Services;
+
+var scanner = new FileScannerService();
+
+Console.Write("Ordner: ");
+
+var path = Console.ReadLine();
+
+scanner.SearchFiles(path!);
---
+++ LocalKnowledgeAssistant/Services/FileScannerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalKnowledgeAssistant.Services
+{
+    internal class FileScannerService
+    {
+        public void SearchFiles(string path)
+        {
+            var files = Directory.GetFiles(path,"*.PNG",
+                SearchOption.AllDirectories);
+            foreach(var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
